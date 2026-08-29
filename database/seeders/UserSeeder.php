c2cf48cb1e956d224710edb34b5bdb2d36cd1842
ahmedgad 112 <gadoo007551@gmail.com>
2026-08-29 04:13:00 +0300

    add user and roul seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,11 +4,16 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        if (Role::query()->where('guard_name', 'web')->doesntExist()) {
+            $this->call(RolePermissionSeeder::class);
+        }
+
         $users = [
             ['name' => 'أحمد المدير', 'email' => 'admin@boshret.test', 'phone' => '01000000001', 'role' => 'المدير العام'],
             ['name' => 'سارة العقارية', 'email' => 'properties@boshret.test', 'phone' => '01000000002', 'role' => 'مدير العقارات'],
@@ -17,18 +22,25 @@ class UserSeeder extends Seeder
             ['name' => 'خالد الموظف', 'email' => 'staff@boshret.test', 'phone' => '01000000005', 'role' => 'موظف عادي'],
         ];
 
+        $seedEmails = collect($users)->pluck('email');
+
         foreach ($users as $data) {
-            $user = User::query()->updateOrCreate(
+            $user = User::withTrashed()->updateOrCreate(
                 ['email' => $data['email']],
                 [
                     'name' => $data['name'],
                     'phone' => $data['phone'],
                     'password' => '12345678',
                     'is_active' => true,
+                    'deleted_at' => null,
                 ]
             );
 
             $user->syncRoles([$data['role']]);
         }
+
+        User::query()
+            ->whereNotIn('email', $seedEmails)
+            ->delete();
     }
 }
